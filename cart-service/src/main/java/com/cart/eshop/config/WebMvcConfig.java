3b37dc2befead52270f0c4c798884f30ad47a8c3
zhanyd <zhanyd@sina.com>
2018-09-09 21:03:03 +0800

    add

---
+++ cart-service/src/main/java/com/cart/eshop/config/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.cart.eshop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by zhanyd@sina.com on 2018/2/16 0016.
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+    /**
+     * 添加过滤链
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new PermissionInterceptor()).addPathPatterns("/**")
+                .excludePathPatterns("/error","/user/isUserExist","/user/login","/");
+    }
+}
