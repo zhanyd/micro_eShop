d0f15898da8a54163621673a826a009b8fcfc1f9
zhanyd <zhanyd@sina.com>
2018-10-10 21:50:50 +0800

    Add

--- order-service/src/main/java/com/order/eshop/biz/controller/OrderController.java
+++ order-service/src/main/java/com/order/eshop/biz/controller/OrderController.java
@@ -2,10 +2,16 @@ package com.order.eshop.biz.controller;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.order.eshop.biz.service.OrderService;
 import com.order.eshop.common.ApiResult;
 import feign.Param;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 /**
  * Created by Administrator on 2018/9/14 0014.
@@ -15,6 +21,9 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/order")
 public class OrderController {
 
+    @Autowired
+    OrderService orderService;
+
     @PostMapping("addOrder")
     public ApiResult<String> addProduct(@RequestBody String productListJSonStr){
         ApiResult<String> result = new ApiResult<String>();
@@ -22,6 +31,13 @@ public class OrderController {
         JSONObject parseObject = JSONObject.parseObject(productListJSonStr);
         Integer userId = parseObject.getInteger("userId");
         JSONArray productList = parseObject.getJSONArray("productList");
+
+        Random ran = new Random();
+        Map<String,Object> param = new HashMap<String,Object>();
+        param.put("orderNum", userId + "" + ran.nextInt(1000));
+        param.put("userId",userId);
+        param.put("productArrayList",productList);
+        orderService.insertByProductArrayList(param);
         return result.success("添加到订单");
     }
 }
--- order-service/src/main/java/com/order/eshop/biz/mapper/OrderInfoMapper.java
+++ order-service/src/main/java/com/order/eshop/biz/mapper/OrderInfoMapper.java
@@ -2,6 +2,8 @@ package com.order.eshop.biz.mapper;
 
 import com.order.eshop.biz.model.OrderInfo;
 
+import java.util.Map;
+
 public interface OrderInfoMapper {
     int deleteByPrimaryKey(Integer id);
 
@@ -14,4 +16,6 @@ public interface OrderInfoMapper {
     int updateByPrimaryKeySelective(OrderInfo record);
 
     int updateByPrimaryKey(OrderInfo record);
+
+    int insertByProductArrayList(Map<String,Object> param);
 }
